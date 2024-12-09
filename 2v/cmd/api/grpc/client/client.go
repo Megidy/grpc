e8d6601d1e201aa22ddc1b9e8a24fac57ca9eb58
Megidy <byrka.sergiy@gmail.com>
2024-12-09 22:42:45 +0200

    Added create grpc client func.

--- 2v/cmd/api/grpc/client/client.go
+++ 2v/cmd/api/grpc/client/client.go
@@ -1,1 +1,14 @@
 package client
+
+import (
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func NewGrpcClient(address string) (*grpc.ClientConn, error) {
+	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
